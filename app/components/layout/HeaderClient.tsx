13a42e129a5380eb315715e5a9fe939aea3dce4f
ashishnamdeo <ashishnamdev14@gmail.com>
2026-01-18 14:17:11 -0800

    tes

--- app/components/layout/HeaderClient.tsx
+++ app/components/layout/HeaderClient.tsx
@@ -6,16 +6,10 @@ import Header from "@/app/components/layout/Header";
 export default function HeaderClient() {
   const [darkMode, setDarkMode] = useState(true);
 
-  // Optional: load saved theme
-  useEffect(() => {
-    const saved = localStorage.getItem("theme");
-    if (saved) setDarkMode(saved === "dark");
-  }, []);
-
   // Apply theme to <html> and persist
   useEffect(() => {
     document.documentElement.classList.toggle("dark", darkMode);
-    localStorage.setItem("theme", darkMode ? "dark" : "light");
+    localStorage.setItem("theme", "dark" );
   }, [darkMode]);
 
   return <Header darkMode={darkMode} setDarkMode={setDarkMode} />;
